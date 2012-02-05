efca79c86b3ae709912d7288e7f67cf849ca35ef
Jason O'Neil <jason.oneil@gmail.com>
2012-02-06 02:50:09 +0800

    Initial commit.  Basic python and bash file, can run and update git, and autodetect proxy.

---
+++ startup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+
+# A simple function to launch a command
+def runCommand(cmd):
+	sys.stderr = sys.stdout
+	args = cmd.split(" ")
+	proc = error=subprocess.Popen(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+	if sys.platform == "win32": 
+		proc.communicate()[0].decode("windows-1252")
+	else:
+		proc.communicate()[0].decode("utf8")
+
+	output = ""
+	#for line in proc.stdout:
+	#	output = output + "stdout: " + line.decode("windows-1252").rstrip() + "\n"
+	#for line in proc.stderr:
+	#	output = output + "stderr: " + line.decode("windows-1252").rstrip() + "\n"
+	
+	return_code = proc.wait()
+	return return_code
+	
+	pass
+
+# Some simple variables we can use
+home = os.getenv('USERPROFILE') or os.getenv('HOME')
+networkDir = home + "/NetworkDrives/"
+pwsAppDir = home + "/.pws/"
+
+# Launch commands depending on the platform...
+if sys.platform == "linux2":
+	runCommand("./update.sh " + pwsAppDir + "School-Logon")
+	#runCommand("dolphin .");
+	#runCommand("git pull");
+elif sys.platform == "win32": 
+	pass
+elif sys.platform == "darwin":
+	runCommand("open .");
+	runCommand("git pull");
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Check if a port is open, and if so, use a proxy
+# Taken from http://www.carlowens.me/2010/04/bash-script-to-check-for-open-port/
+
+HOSTIP='192.168.0.1'
+PORT='3128'
+
+if nc -zv -w10 $HOSTIP $PORT
+then
+# Set the proxy
+export http_proxy=http://$HOSTIP:$PORT
+export https_proxy=http://$HOSTIP:$PORT
+fi
+
+# Now move into the directory and try to pull it down.
+cd $1
+git pull
